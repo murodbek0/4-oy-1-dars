68dfe25faf22517f04ac4a0cecb09955ebeee40e
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-05-17 17:45:35 +0500

    11 issuse

---
+++ js/script.js
@@ -0,0 +1,88 @@
+// 1-misol
+// let L = +prompt("uzunlikni sm larda kriting");
+// let m = Math.floor(L/100);
+// console.log(m);
+
+// 2-misol
+// let M = +prompt("og'rligini kg da kriting");
+// let t = Math.floor(M/1000);
+// console.log(t);
+
+// 3-misol
+// let B = +prompt("baytlarda bolsin");
+// let a = Math.floor(1024/B)
+// console.log(a)
+
+
+// 4-misol
+// alert('A>B');
+// let B = +prompt("");
+// let A = +prompt("");
+// let a = Math.floor(A/B);
+// console.log(a);
+
+// 5-misol
+
+// alert('A>B');
+// let B = +prompt("")
+// let A = +prompt("")
+// let m = Math.floor(A%B)
+// console.log(m)
+
+// 6-misol
+
+// alert('2 honali son kriting');
+// let A = +prompt("A");
+// let H = Math.floor(A/10);
+// console.log(H);
+// let v = Math.floor(A%10);
+// console.log(v);
+
+// 7-misol
+
+// alert('2 honali son kriting');
+// let A = +prompt("A");
+// let H = Math.floor(A/10);
+// console.log(H);
+// let v = Math.floor(A%10);
+// console.log(v);
+// let P = Math.floor(v+H);
+// console.log(P);
+
+
+
+// 8-misol
+
+// alert('2 honali son kriting');
+// let B = +prompt("B");
+// let N = Math.floor(B/10);
+// let C = Math.floor(B%10);
+// let D = Math.floor((C*10)+N);
+// console.log(D);
+
+
+// 9-misol
+//  alert('3 honali son krting');
+//  let s = +prompt('s');
+//  let m = Math.floor(s/100);
+//  console.log (m);
+
+
+// 10-masala
+
+//  alert('3 honali son krting');
+// let B = +prompt("B");
+// let a = Math.floor(B%10);
+// let z = Math.floor(B/10%10);
+// console.log(a);
+// console.log(z);
+
+
+// 13 - misol
+
+// alert('3 honali son berrilgan')
+// let B = +prompt("B");
+// let b = Math.floor(B/100);
+// let o = Math.floor(B/10%10);
+// let y = Math.floor(B%10);
+// console.log((o*100)+(y*10)+b);
